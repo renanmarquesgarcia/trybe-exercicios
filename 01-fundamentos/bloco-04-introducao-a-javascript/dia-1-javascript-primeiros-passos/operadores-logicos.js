c598b164f0925f32bc3f9472127561e2969a035a
Renan Marques <nan.dev01@gmail.com>
2022-09-21 18:09:21 -0300

    Exercício para fixar de operadores lógicos

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/operadores-logicos.js
@@ -0,0 +1,51 @@
+// const currentHour = 12;
+// let message="";
+
+// if(currentHour >= 22){
+//     message = "Não deveríamos comer nada, é hora de dormir” na variável"
+// }
+
+// else if (currentHour >=18 && currentHour < 22){
+//     message = "Rango da noite, vamos jantar :D"
+// }
+
+// else if (currentHour >14 && currentHour < 18){
+//     message = "Vamos fazer um bolo pro café da tarde?"
+// }
+
+// else if(currentHour >= 11 && currentHour < 14){
+//     message = "Hora do almoço!!!"
+// }
+
+// else{
+//     message = "Hmmm, cheiro de café recém-passado” na variável"
+// }
+
+// console.log(message);
+
+
+// let weekDay = "quarta-feira"
+
+// if(weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira"){
+//     console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+// }
+
+// else{
+//     console.log("FINALMENTE, descanso merecido UwU");
+// }
+
+console.log(!(2 + 2) === 4); // false
+
+const squirtle = "melhor pokemon inicial";
+console.log(!squirtle); // false
+
+console.log(!42); // false
+
+console.log(!0); // true
+// O número 0 tem o valor "falsy" no javascript. Logo, seu oposto é true.
+
+console.log(!null); // true
+
+console.log(!undefined); // true
+
+
