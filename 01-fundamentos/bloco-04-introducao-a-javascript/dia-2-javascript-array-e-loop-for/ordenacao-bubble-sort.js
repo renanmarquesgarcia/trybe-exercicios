626aa1cb5cfc3b786069e76defeaf538266a3f7e
Renan Marques <nan.dev01@gmail.com>
2022-09-23 18:42:48 -0300

    Exercícios 1 e 2 resolvidos

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/ordenacao-bubble-sort.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/ordenacao-bubble-sort.js
@@ -1,23 +1,23 @@
-// 1 - Ordene o array numbers em ordem crescente e imprima seus valores;
+// // 1 - Ordene o array numbers em ordem crescente e imprima seus valores;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 
-//Vamos ter que percorrer toda a array, logo:
-for(let index = 1; index < numbers.length; index += 1){
-    // Dentro de cada iteração
-    // percorremos novamente o vetor
-    // em busca dos pares
-    for(let secondIndex = 0; secondIndex < index; secondIndex += 1){
-        // Comparamos
-        if (numbers[index] < numbers[secondIndex]) {
-            // Swap (trocamos)
-            let position = numbers[index];
-            numbers[index] = numbers[secondIndex];
-            numbers[secondIndex] = position;
+// //Vamos ter que percorrer toda a array, logo:
+// for(let index = 1; index < numbers.length; index += 1){
+//     // Dentro de cada iteração
+//     // percorremos novamente o vetor
+//     // em busca dos pares
+//     for(let secondIndex = 0; secondIndex < index; secondIndex += 1){
+//         // Comparamos
+//         if (numbers[index] < numbers[secondIndex]) {
+//             // Swap (trocamos)
+//             let position = numbers[index];
+//             numbers[index] = numbers[secondIndex];
+//             numbers[secondIndex] = position;
                
-        }
-    }  
-}
+//         }
+//     }  
+// }
 
-console.log(numbers);
+// console.log(numbers);
