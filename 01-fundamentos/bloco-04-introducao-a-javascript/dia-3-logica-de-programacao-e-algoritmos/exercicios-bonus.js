b70cde97c8fe34d8d8876317de04c2ce7e4191de
Renan Marques <nan.dev01@gmail.com>
2022-09-26 18:28:34 -0300

    Exercícios para fixar resolvidos

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-3-logica-de-programacao-e-algoritmos/exercicios-bonus.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-3-logica-de-programacao-e-algoritmos/exercicios-bonus.js
@@ -2,17 +2,19 @@
 
 // 1 - Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
 
-// let n = 5;
-// let asteriskLine = ''
+// let n = 5
+// let asteriskLine = '';
 
-// for(let contador = 1; contador <= n; contador += 1){
-//     asteriskLine += '*';
+// for(let index = 1; index <= n; index += 1){
+//     asteriskLine += '*'
 // }
-// for(let contador = 1; contador <= n; contador += 1){
+// for(let index = 1; index <= n; index += 1){
 //     console.log(asteriskLine);
 // }
+  
 
 //=========================================================================
+
 
 // 2 - Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
 
@@ -24,9 +26,15 @@
 //     console.log(asteriskLine);
 // }
 
+
 //=========================================================================
 
-// 3 - 
+
+// 3 - Agora inverta o lado do triângulo.
+
+
+
+
 
 
 
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/funcoes.js
@@ -0,0 +1,87 @@
+// 1 -Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+// function calculadora (a, b, operador){
+//     if (operador === '+'){
+//         return a + b;
+//     } else if (operador === '-'){
+//         return a - b;
+//     } else if (operador === '*'){
+//         return a * b;
+//     } else if (operador === '/'){
+//         return a / b;
+//     } else if (operador === '%'){
+//         return a % b;
+//     }
+// }
+
+
+//=========================================================================
+
+
+// 2 - Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+
+// function biggestNumber(a, b){
+        
+//     if (a > b){
+//         return a + ': a é o maior número'
+//     } else if (b > a){
+//         return b + ': b é o maior número'
+//     } else {
+//         return 'Os números são iguais'
+//     }
+    
+// }
+
+
+//=========================================================================
+
+// 3 - Faça um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
+
+function biggestNumber(a, b, c){
+    
+    if(a > b && a > c){
+        return a + ': a é o maior número'
+    } else if (b > a && b > c){
+        return b + ': b é o maior número'
+    } else {
+        return c + ': c é o maior número'
+    }
+}
+
+
+//=========================================================================
+
+// 4 - Faça um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
+// function positiveNegative (num) {
+//     if (num > 0){
+//         return 'positive';
+//     }
+//     else if (num < 0){
+//         return 'negative';
+//     }
+//     else {
+//         return 'zero';
+//     }
+// }
+
+
+//=========================================================================
+
+// 5 - Faça um programa que receba três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+
+function isTriangle(angleA, angleB, angleC){
+    if(angleA < 0 || angleB < 0 || angleC < 0){
+        return 'error, insira valores positivos'
+    } else if (angleA + angleB + angleC === 180){
+        return true;
+    }
+    else {
+        return false
+    }
+}
+
+
+
+
+
