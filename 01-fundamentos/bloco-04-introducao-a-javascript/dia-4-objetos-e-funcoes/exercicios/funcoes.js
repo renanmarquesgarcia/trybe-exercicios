edf7b908a66687bce413ebd62476508ec818ee62
Renan Marques <nan.dev01@gmail.com>
2022-09-27 11:35:58 -0300

    Exercício funções 2 e 3 resolvidos

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/exercicios/funcoes.js
@@ -0,0 +1,50 @@
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
+
+function palindrome(word){
+
+}
+
+
+//=========================================================================
+
+
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+// function biggestValueIndex(arrayNum){
+//     let indexMaxValue = 0;
+        
+//     for (let index in arrayNum){
+//         if(arrayNum[index] > arrayNum[indexMaxValue]){
+//         indexMaxValue = index
+//         }    
+//     }
+//     return indexMaxValue;
+// }
+// console.log(biggestValueIndex([2, 3, 6, 7, 10, 1]));
+
+
+//=========================================================================
+
+
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+// function smallestValueIndex(arrayNum){
+//     let indexMinValue = 0;
+        
+//     for (let index in arrayNum){
+//         if(arrayNum[index] < arrayNum[indexMinValue]){
+//         indexMinValue = index
+//         }    
+//     }
+//     return indexMinValue;
+// }
+// console.log(smallestValueIndex([2, 4, 6, 7, 10, 0, -3]));
+
+
+//=========================================================================
+
+
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+
+
--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/exercicios/objetos-e-for-in.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/exercicios/objetos-e-for-in.js
@@ -89,18 +89,18 @@
 
 // 6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
 
-let leitor = {
-  nome: 'Julia',
-  sobrenome: 'Pessoa',
-  idade: 21,
-  livrosFavoritos: [
-    {
-      titulo: 'O Pior Dia de Todos',
-      autor: 'Daniela Kopsch',
-      editora: 'Tordesilhas',
-    },
-  ],
-};
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//   ],
+// };
 
 // console.log ('O livro favorito de ' + leitor.nome, leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
 
@@ -112,13 +112,13 @@ let leitor = {
 
 // 7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto contendo as seguintes informações:
 
-leitor.livrosFavoritos.push(
-  {
-  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-  autor: 'JK Rowling',
-  editora: 'Rocco',
-  },
-);
+// leitor.livrosFavoritos.push(
+//   {
+//   titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//   autor: 'JK Rowling',
+//   editora: 'Rocco',
+//   },
+// );
 
 // console.log(leitor.livrosFavoritos[leitor.livrosFavoritos.length - 1]);
 
@@ -128,4 +128,4 @@ leitor.livrosFavoritos.push(
 
 // 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código.
 
-console.log('"' + leitor['nome'] + ' tem ' + leitor['livrosFavoritos'].length + ' livros favoritos' + '"');
+// console.log('"' + leitor['nome'] + ' tem ' + leitor['livrosFavoritos'].length + ' livros favoritos' + '"');
