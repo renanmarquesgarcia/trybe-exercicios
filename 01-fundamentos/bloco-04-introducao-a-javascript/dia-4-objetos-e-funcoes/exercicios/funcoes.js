f36b91efb77559fa7cec997b988054428343ae73
Renan Marques <nan.dev01@gmail.com>
2022-09-27 12:15:21 -0300

    Exercícios funções 4 resolvido

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/exercicios/funcoes.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-4-objetos-e-funcoes/exercicios/funcoes.js
@@ -46,5 +46,26 @@ function palindrome(word){
 
 // 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
+// function biggestName(names){
+    
+//     let biggestName = names[0]
+
+//     for(let index in names){
+//         if(names[index].length > biggestName.length){
+//             biggestName = names[index]
+//         }
+//     }
+//     return biggestName;
+// }
+
+// console.log(biggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 
 
+//=========================================================================
+
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+function mostTimeRepeated(numbers){
+    
+}
