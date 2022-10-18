9565ad59aefa638814c4e4b633fcf591142be93f
Renan Marques <nan.dev01@gmail.com>
2022-10-18 12:09:07 -0300

    Exercícios 7.1 corrigidos

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/script.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/script.js
@@ -42,7 +42,6 @@ const testingScope = (escopo) => {
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 const sortOddsAndEvens = () => {
-
 const sort = oddsAndEvens.sort((a,b) => a - b);
 return sort
 };
@@ -52,25 +51,47 @@ console.log(`Os números ${sortOddsAndEvens()} se encontram ordenados de forma c
 // ----------------------------------------------------------------
 // Crie uma função que receba um número e retorne seu fatorial.
 
-const factorial = (number) => {
-    let result = 1;
-    for (let index = 2; index <= number; index += 1){
-        result *= index;
-    }
-    console.log(`Esse é o fatorial ${result}`);
-}
+// Solução 1
+// const factorial = (number) => {
+//     let result = 1;
+//     for (let index = 2; index <= number; index += 1){
+//         result *= index;
+//     }
+//     return (`Esse é o fatorial ${result}`);
 
-factorial(4);
+// }
+
+// console.log(factorial(4));
+
+// Solução 2
+const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
+console.log(factorial(4));
 
 // -----------------------------------------------------------------
 // Crie uma função que receba uma frase como parâmetro e retorne a maior palavra contida nesta frase.
 
+// Solução 1 
+// const longestWord = (phrase) => {
+//   const arrayPhrase = phrase.split(' ')
+//   let maxLength = 0;
+//   let result = ''
+
+//   for(const word of arrayPhrase){
+//     if (word.length > maxLength ) {
+//       maxLength = word.length;
+//       result = word;
+//     }
+//   }
+//   return result;
+// }
+
+// Solução 2
 const longestWord = (phrase) => {
     const arrayPhrase = phrase.split(' ');
     return arrayPhrase.sort((a, b) => b.length - a.length)[0]
 }
 
-console.log(longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu')) // retorna 'aconteceu'
+console.log(longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu')) 
 
 // -----------------------------------------------------------------
 // Escreva uma função que substitua a letra ‘x’ em uma frase.
@@ -80,35 +101,28 @@ const substituaX = (nome) => {
   const arrayFrase = frase.split(' ')  
   
   for(let index = 0; index < arrayFrase.length; index += 1){
-    if(index === 1){
+    if(arrayFrase[index] === 'x'){
       arrayFrase[index] = nome;
-      return (arrayFrase.join(' '))
+      
     }
   }
-  
+  return (arrayFrase.join(' '))
 }
 console.log(substituaX('Renan'));
-
-
-// const substituaX = (nome) => {
-//   const frase = 'Tryber x aqui!'
-//   let newPhrase = ''
-
-//   for(let index = 0; index < frase.length; index += 1){
-//     frase[index] === 'x' ? `${frase[index]} = ${nome}` : `${frase[index]} = ${frase[index]}`
-//     newPhrase.push(frase[index]);
-//   }
-//   return newPhrase;
-// }
-// console.log(substituaX('Renan'));
 
 // -----------------------------------------------------------------------
 // Escreva uma função que irá receber o retorno da Função 1 por parâmetro e retornará uma nova string.
 
 const minhasSkills = (funcao1) => {
   const skills = ['JavaScript', 'HTML', 'CSS']
-  let concatena = `${funcao1} Minhas três principais habilidades são: ${skills[0]}, ${skills[1]} e ${skills[2]}.`
-  return concatena
+  let concatena = `${funcao1}\nMinhas três principais habilidades são:`;
+
+  for (let index = 0; index < skills.length; index += 1) {
+    concatena = `${concatena}
+  - ${skills[index]}`;    
+  }
+  
+  return concatena;
 }
 
 console.log(minhasSkills(substituaX('Renan')));
